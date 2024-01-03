898b4160e0447e2b34ef95979f74e8cabf45bc82
prem sargara <premraju943@gmail.com>
2024-01-03 11:37:26 +0000

    deleted

--- healthdocs/db/forgot-pw.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    reset_token VARCHAR(255),
-    reset_token_expires_at DATETIME
-);
--- healthdocs/db/medicine.sql
+++
@@ -1,8 +0,0 @@
--- creating table for medicines
-CREATE TABLE medicines (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2),
-    purchase_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
